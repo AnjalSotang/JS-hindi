a76c804c3ee7e0df95824da2e495db5d4126ca63
AnjalSotang <137472810+AnjalSotang@users.noreply.github.com>
2024-06-11 06:34:24 +0000

    Basics of varibale description

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,5 @@
-console.log("Anjal")
+//Figuring out the concept of let, Const, and Var
+// In JS variable allow system to store or provide memory sapace to store data for a short period of time. It can be declared using three key words let, const, and var.
+//Syntaxs are defined in the compiler such as (Node.js or V8 engine) by the Developer For Example if console.log is runed the compiler will display the result of the program in the screen
+//By the inorder to display many variable together we can use console.table([variable_name, variable_na
+//Constant variable which is immutable can only by declared using the keyword const but to define the mutable variable we can use either var or let but, suggestion is let instead of var because var has scope problem issues from the beiginning.
