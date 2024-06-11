8ff446dd72514551acb3ab5712028a03cf80072b
AnjalSotang <137472810+AnjalSotang@users.noreply.github.com>
2024-06-11 08:42:17 +0000

    Conversion of datatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,25 @@
+// let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber));
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+
+// let boolenIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boolenIsLoggedIn)
+
+// 1 => true; 0 => false
+//"" => false
+// "hitesh" => true
+
+let someNumber = "33"
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
